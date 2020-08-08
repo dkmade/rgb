fe6f32e9566eebf8c0b442add8a62c277d3137e2
Kochetkov <dk@made.ru.com>
2020-08-08 19:19:49 +0300

    WIP

--- py/rgbled.py
+++ py/rgbled.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 #importing the library for delaying command.
 import time
 import json
+import datetime
 
 #used for GPIO numbering
 GPIO.setmode(GPIO.BCM) 
@@ -68,9 +69,40 @@ try:
 #                         print data['g']
 #                         print data['b']
 
-                        RED.ChangeDutyCycle(data['rgb1']['r'])
-                        GREEN.ChangeDutyCycle(data['rgb1']['g'])
-                        BLUE.ChangeDutyCycle(data['rgb1']['b'])
+
+                        datetime_on_start = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d ') + data['timeOn'] + ':00', '%Y-%m-%d %H:%M:%S')
+                        datetime_on_end   = datetime_on_start + datetime.timedelta(minutes = data['timeOfGradient'])
+                        datetime_off_end   = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d ') + data['timeOff'] + ':00', '%Y-%m-%d %H:%M:%S')
+                        datetime_off_start   = datetime_off_end - datetime.timedelta(minutes = data['timeOfGradient'])
+                        delta_all = data['timeOfGradient'] * 60
+
+                        if (datetime.datetime.now() < datetime_on_start or datetime.datetime.now() > datetime_off_end):
+                            RED.ChangeDutyCycle(0)
+                            GREEN.ChangeDutyCycle(0)
+                            BLUE.ChangeDutyCycle(0)
+                        elif(datetime.datetime.now() > datetime_on_start and datetime.datetime.now() < datetime_on_end):
+                            delta_now = datetime.datetime.now() - datetime_on_start
+                            seconds = delta_now.total_seconds()
+                            koef = seconds/delta_all
+                            RED.ChangeDutyCycle(data['rgb1']['r']*koef)
+                            GREEN.ChangeDutyCycle(data['rgb1']['g']*koef)
+                            BLUE.ChangeDutyCycle(data['rgb1']['b']*koef)
+                        elif(datetime.datetime.now() > datetime_on_end and datetime.datetime.now() < datetime_off_start):
+                            RED.ChangeDutyCycle(data['rgb1']['r'])
+                            GREEN.ChangeDutyCycle(data['rgb1']['g'])
+                            BLUE.ChangeDutyCycle(data['rgb1']['b'])
+                        elif(datetime.datetime.now() > datetime_off_start and datetime.datetime.now() < datetime_off_end):
+                            delta_now = datetime.datetime.now() - datetime_on_start
+                            seconds = delta_now.total_seconds()
+                            koef = 1 - seconds/delta_all
+                            RED.ChangeDutyCycle(data['rgb1']['r']*koef)
+                            GREEN.ChangeDutyCycle(data['rgb1']['g']*koef)
+                            BLUE.ChangeDutyCycle(data['rgb1']['b']*koef)
+
+
+#                         RED.ChangeDutyCycle(data['rgb1']['r'])
+#                         GREEN.ChangeDutyCycle(data['rgb1']['g'])
+#                         BLUE.ChangeDutyCycle(data['rgb1']['b'])
                         time.sleep(0.1)
 
 except KeyboardInterrupt:
